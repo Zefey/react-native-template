df77c43095b530ce3bfb9a7af5e1a2d7ab5f7879
zefey <1076971426@qq.com>
2019-11-09 22:31:12 +0800

    update

--- ios/temple/AppDelegate.m
+++ ios/temple/AppDelegate.m
@@ -10,11 +10,14 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+//  [RNSplashScreen show];
+  
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"temple"
